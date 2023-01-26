40c67dd9ce37508c852da26a05580e49208443b7
BernhardRy <bernhard.ry@gmail.com>
2023-01-26 11:27:50 +0100

    Startet med web scraping av VG

--- bs4_web_scraping_01.py
+++ bs4_web_scraping_01.py
@@ -2,14 +2,24 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-url = "https://www.vg.no/nyheter/innenriks/i/P4j4lz/kjerkol-om-omsorgssvikt-blir-like-opproert-som-alle-andre"
+url = "https://www.vg.no/"
 
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
 
-tag = doc.find_all(text=re.compile("Kjerkol.*"))
+#tag = doc.find_all(text=re.compile("Kjerkol.*"))
 #tag = doc.find_all(["article"], class_="article")
 # parent = prices[0].parent
 
 # print(doc.prettify())
-print(tag)
+
+tags = doc.find_all(['a'], href=True)
+
+for i in tags:
+    if "https" in i["href"]:
+        url = i["href"]
+
+        if "kjerkol-" in url.lower():
+            print(url)
+
+# print(tags[0])
